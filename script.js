b7356e899f091376d9215e2fa2b34b458ba9e5b5
MAHADI ABUHURAIRA <mamt4real@gmail.com>
2022-04-28 17:14:34 +0100

    commenting the script

--- script.js
+++ script.js
@@ -8,39 +8,46 @@ function placeChildrens(
   const angleChange = 360 / noChildrens
 
   for (let i = 0; i < noChildrens; i++) {
+    //compute angular displacement of the next child
     const angle = (initialOffset + (angleChange * i * Math.PI) / 180) % 360
 
+    //obtain the vertical and horizontal component
     const dX = Math.sin(angle)
     const dY = Math.cos(angle)
+
+    //variables to store the left and top values of css property
     var leftOffset = 0
     var topOffset = 0
 
     switch (true) {
+      //top right segment (1st quadrant) x+ y+
       case 0 <= angle && angle < 90: {
         leftOffset = parentRadius * (1 + dX)
         topOffset = parentRadius * (1 - dY)
         break
       }
+      // botom right sengment (4th quadrant) x- y+
       case 90 <= angle && angle < 180: {
         leftOffset = parentRadius * (2 - dX)
         topOffset = parentRadius * (1 + dY)
         break
       }
+      // botom left sengment (3rd quadrant) x- y-
       case 180 <= angle && angle < 270: {
         leftOffset = parentRadius * (1 - dX)
         topOffset = parentRadius * (2 - dY)
         break
       }
-
+      // top left sengment (2nd quadrant) x+ y-
       case 270 <= angle && angle <= 360: {
         leftOffset = parentRadius * dX
         topOffset = parentRadius * (1 - dY)
       }
     }
+
     const child = getChild(childSize, i)
     child.style.top = `${topOffset}px`
     child.style.left = `${leftOffset}px`
-    // console.log(child)
     parent.appendChild(child)
   }
 }
@@ -53,14 +60,14 @@ function placeCentroids(
 ) {
   const cont = document.getElementById(id)
 
+  //get Radius of the container
   const height = Math.max(cont.clientHeight, cont.clientWidth)
-
   const parentRadius = height / 2
 
-  //remove childrens
-  const childrens = document.querySelectorAll(`#${id} > .child`)
-  childrens.forEach((ch) => ch.remove())
-  // cont.innerHTML = ''
+  //remove old childrens (if any)
+  document.querySelectorAll(`#${id} > .child`).forEach((ch) => ch.remove())
+
+  //place new childrens
   placeChildrens(cont, no, childSize, parentRadius, initialOffset)
 }
 
@@ -78,13 +85,12 @@ function getChild(childSize = 50, index) {
 
 function handleFormSubmit(e) {
   e.preventDefault()
-
   const no = e.target.no.value
-
   placeCentroids('main', no, 80)
   placeCentroids('inner', parseInt((2 * no) / 3), 65, 30)
   placeCentroids('inner2', parseInt(no / 2), 45, 45)
 }
 
 placeCentroids()
+//register form listener
 document.getElementById('form').addEventListener('submit', handleFormSubmit)
